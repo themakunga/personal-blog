b9b2bfecccc3dc860a3dd34d04c895cc67684c7e
Nicolas Martinez <nmartinezv@icloud.com>
2019-09-13 09:06:48 -0300

    Review de Ducktales y Sistema de puntuacion de reviews

---
+++ src/.vuepress/components/Stars.vue
@@ -0,0 +1,35 @@
+<template >
+  <div class="">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <h3>Puntaje: </h3>
+    <div v-html="items" class="">
+
+  </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Stars',
+  data: () => ({
+    max: 5,
+    items: null,
+  }),
+  props: [
+    'amount',
+  ],
+  created() {
+    this.items = '';
+    for (var i = 1; i <= this.max; i++) {
+      if (i <= this.amount) {
+        this.items += '<span class="fa fa-star fa-2x fa-fw checked" style="color: orange;"></span>';
+      } else {
+        this.items += '<span class="fa fa-star fa-2x fa-fw"></span>';
+      }
+    }
+  },
+};
+</script>
---
+++ src/_posts/2019-09-12-sobre_experiencia_autosabotaje_e_impostores.md
@@ -0,0 +1,14 @@
+---
+layout: PostLayout
+title: Sobre Experiencia, Autosabotaje & Impostores
+category: Blog
+date: Thu, 12 Sep 2019 18:26:08 GMT
+tags:
+  - opinion
+  - experiencia
+  - blog
+  - casos de uso
+header-image:
+draft: true
+---
+# Sobre Experiencia, Autosabotaje & Impostores
